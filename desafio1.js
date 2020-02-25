daac6ea57414ea73fbfb2dd90ba44bc5da8f9f28
Vilmara Silva <33360699+vilmarap@users.noreply.github.com>
2020-02-25 17:41:45 -0300

    resolucao do desafio
    resolucao do desafio

---
+++ desafio1.js
@@ -0,0 +1,30 @@
+// // Cálculo de IMC
+
+// const nome = 'Carlos';
+// const peso = 84
+// const altura = 1.88
+
+// const imc = peso / (altura * altura);
+
+// if (imc >= 30) {
+//     console.log('Carlos você está acima do peso')
+// } else {
+//     console.log('Carlos você não está acima do peso')
+// }
+
+// Cálculo da aposentadoria
+
+const nome = "Silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
+
+const soma = idade + contribuicao;
+
+// se a pessoa estiver aposentada
+if (soma >= 85) { 
+    console.log(`${nome}, você pode se aposentar!`)
+} else (soma <= 85); {
+    console.log(`${nome}, você não pode se aposentar!`)
+
+}
