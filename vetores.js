f4774b4fe74d7217b665fb7865106c633cfbad0a
Vilmara Silva <33360699+vilmarap@users.noreply.github.com>
2020-02-27 01:49:56 -0300

    aula de vetores, exemplo pratico

---
+++ vetores.js
@@ -0,0 +1,20 @@
+// criar um programa que calcula a média
+// Arrays / Vetores
+const alunos = [
+    {
+        nome: "Silva",
+        nota: 9.8
+    },
+    {
+        nome: "Diego",
+        nota: 10
+    },
+    {
+        nome: "Fulano",
+        nota: 2
+    }
+]
+
+const nomesDeAlunos = ["Silva", "Diego", "Fulano"]
+console.log(alunos)
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
